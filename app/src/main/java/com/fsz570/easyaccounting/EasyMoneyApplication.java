8a929e3f66db4522439208dd9fbf2eeb65eca862
David Wu <fsz570@gmail.com>
2015-03-16 12:12:25 +0800

    Update EasyMoneyApplication.java

--- app/src/main/java/com/fsz570/easyaccounting/EasyMoneyApplication.java
+++ app/src/main/java/com/fsz570/easyaccounting/EasyMoneyApplication.java
@@ -1,1 +1,49 @@
-test
+package com.fsz570.easyaccounting;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.fsz570.db_utils.DBAdapter;
+
+public class EasyMoneyApplication extends Application{
+
+    private static String TAG = "EasyMoneyApplication";
+    private static DBAdapter dbAdapter = null;
+    private static Context context = null;
+
+    @Override
+    public void onCreate()
+    {
+        Log.d(TAG, "onCreate()");
+        super.onCreate();
+
+        context = this;
+    }
+
+    public static synchronized DBAdapter getDbAdapterInstance(){
+        Log.d(TAG, "getDbAdapterInstance()");
+
+        if(dbAdapter == null) {
+            Log.d(TAG, "create DBAdapter!");
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dbAdapter = DBAdapter.getInstance(context);
+                }
+            });
+            t.start();
+
+            try {
+                Log.d(TAG, "Before Join!");
+                t.join();
+                Log.d(TAG, "After Join!");
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        return dbAdapter;
+    }
+}
